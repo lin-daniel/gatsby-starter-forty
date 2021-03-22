73ebfde176b45187a9d195c9800a4e560e5103d2
Daniel Lin <daniellin256@gmail.com>
2021-03-21 20:59:11 -0700

    Modified Title

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,13 +4,12 @@ const Banner = (props) => (
     <section id="banner" className="major">
         <div className="inner">
             <header className="major">
-                <h1>Hi, my name is Forty</h1>
+                <h1>The Art of Water</h1>
             </header>
             <div className="content">
-                <p>A responsive site template designed by HTML5 UP<br />
-                and released under the Creative Commons.</p>
+                <p>A virtual art exhibit hosted by the Knowing Water Project.</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="#one" className="button next scrolly">Explore</a></li>
                 </ul>
             </div>
         </div>
