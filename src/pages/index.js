dba0aa4eb09f0376aaeb3344e8a0700514bc01c3
Daniel Lin <daniellin256@gmail.com>
2021-03-23 00:47:52 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,7 @@ class HomeIndex extends React.Component {
                     <section id="two">
                         <div className="inner">
                             <header className="major">
-                                <h2>Contribute your art</h2>
+                                <h2>Contribute your art today</h2>
                             </header>
                             <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet pharetra et feugiat tempus.</p>
                             <ul className="actions">
