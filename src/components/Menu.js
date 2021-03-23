e58d4df76926c816fbabbf61da27ea016cbcaaf0
Daniel Lin <daniellin256@gmail.com>
2021-03-22 22:30:23 -0700

    Modified menu and made upload page

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,14 +7,14 @@ const Menu = (props) => (
         <div className="inner">
             <ul className="links">
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/generic">Generic</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/elements">Elements</Link></li>
+                {/* <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li> */}
+                <li><Link onClick={props.onToggleMenu} to="/elements">About the project</Link></li>
+                <li><Link onClick={props.onToggleMenu} to="/upload">Contribute your art</Link></li>
             </ul>
-            <ul className="actions vertical">
-                <li><a href="#" className="button special fit">Get Started</a></li>
+            {/* <ul className="actions vertical">
+                <li><a href="#" className="button special fit">Contribute your art</a></li>
                 <li><a href="#" className="button fit">Log In</a></li>
-            </ul>
+            </ul> */}
         </div>
         <a className="close" onClick={props.onToggleMenu} href="javascript:;">Close</a>
     </nav>
