458431a2571deac4b7bdd5f49801c0f7156fca20
Daniel Lin <daniellin256@gmail.com>
2021-04-30 09:33:55 -0700

    Removed Contributions from menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -9,7 +9,7 @@ const Menu = (props) => (
                 <li><Link onClick={props.onToggleMenu} to="/">Home</Link></li>
                 {/* <li><Link onClick={props.onToggleMenu} to="/landing">Landing</Link></li> */}
                 <li><Link onClick={props.onToggleMenu} to="/elements">About the project</Link></li>
-                <li><Link onClick={props.onToggleMenu} to="/upload">Contribute your art</Link></li>
+                {/* <li><Link onClick={props.onToggleMenu} to="/upload">Contribute your art</Link></li> */}
             </ul>
             {/* <ul className="actions vertical">
                 <li><a href="#" className="button special fit">Contribute your art</a></li>
