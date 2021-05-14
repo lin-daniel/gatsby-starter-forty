8a5c267e45c5739d5dac53bc918489df66025021
Daniel Lin <daniellin256@gmail.com>
2021-05-13 23:59:04 -0700

    updated landing category images

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 import Banner from '../components/Banner'
 
-import abstract1 from '../assets/images/abstract1.jpg'
-import abstract2 from '../assets/images/abstract2.jpg'
-import abstract3 from '../assets/images/abstract3.jpg'
-import abstract4 from '../assets/images/abstract4.jpg'
-import abstract5 from '../assets/images/abstract5.jpg'
-import abstract6 from '../assets/images/abstract6.jpg'
+import clouds from '../assets/images/clouds.jpg'
+import leaf from '../assets/images/leaf-on-ice.jpg'
+import rocks from '../assets/images/rocks.jpg'
+import sand from '../assets/images/sand.jpg'
+import shadows from '../assets/images/shadows.jpg'
+import soundwave from '../assets/images/soundwave.png'
 
 class HomeIndex extends React.Component {
     render() {
@@ -29,42 +29,42 @@ class HomeIndex extends React.Component {
 
                 <div id="main">
                     <section id="one" className="tiles">
-                        <article style={{backgroundImage: `url(${abstract1})`}}>
+                        <article style={{backgroundImage: `url(${clouds})`}}>
                             <header className="major">
                                 <h3>Music</h3>
                                 <p>Join Boom.Diwan for a special workshop and learn the rhythms of Khaleeji pearl-diving music with musician and Ethnomusicologist Dr. Ghazi Al Mulaifi.</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${abstract2})`}}>
+                        <article style={{backgroundImage: `url(${leaf})`}}>
                             <header className="major">
                                 <h3>Performance</h3>
                                 <p>Students at Zayed University’s College of Arts and Creative Enterprises share a common impulse to critically remix the everyday and the nostalgic.</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${abstract3})`}}>
+                        <article style={{backgroundImage: `url(${rocks})`}}>
                             <header className="major">
                                 <h3>Words</h3>
                                 <p>Grassroots art collecting and research platform 101 takes over Warehouse 44 to present its second curated sale "Playing Spaces."</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${abstract4})`}}>
+                        <article style={{backgroundImage: `url(${sand})`}}>
                             <header className="major">
                                 <h3>Art</h3>
                                 <p>How can an exterior environment embody the qualities of interiors, and conversely, an interior integrate exterior characteristics?</p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${abstract5})`}}>
+                        <article style={{backgroundImage: `url(${shadows})`}}>
                             <header className="major">
                                 <h3>Photography</h3>
                                 <p>A roundtable discussion on how to formulate a considered response and blueprint to the question, "how will we return?" </p>
                             </header>
                             <Link to="/landing" className="link primary"></Link>
                         </article>
-                        <article style={{backgroundImage: `url(${abstract6})`}}>
+                        <article style={{backgroundImage: `url(${soundwave})`}}>
                             <header className="major">
                                 <h3>Film</h3>
                                 <p>A conversation with Yemeni-Bosnian-US multi-media artist Alia Ali, curator Miranda McKee, and Mohamed Somji, Director and curator at Gulf Photo Plus.</p>
